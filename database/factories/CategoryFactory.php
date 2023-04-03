db7bd01aa08b75f671d0e9b3a45413438ab099c7
Luis Monteiro <lmonteiro@w-4.ch>
2023-04-03 20:46:04 -0300

    Added Category Factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->text(12),
+            'slug' => fake()->slug,
+            'description' => fake()->text,
+            'image' => fake()->image(),
+            'status' => fake()->boolean,
+        ];
+    }
+}
