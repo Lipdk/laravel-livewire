1652228e32e543309e44a2383b4ede2523185797
Luis Monteiro <lmonteiro@w-4.ch>
2023-04-05 20:33:38 -0300

    Added Pikaday to Vite dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,12 @@
 import './bootstrap';
 
+// import moment from 'moment';
+// window.moment = moment;
+
+import Pikaday from 'pikaday';
+window.Pikaday = Pikaday;
+import "pikaday/css/pikaday.css";
+
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import 'flowbite';
